26e7d1209d695a6882390aa82e9400f573f627f3
Bhawani Singh <Bhawani.Singh@fiftyfivetech.io>
2020-10-10 14:33:16 +0530

    Initial commit

---
+++ react/Bhawani/hackathon-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        <h1>Hej världen</h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
